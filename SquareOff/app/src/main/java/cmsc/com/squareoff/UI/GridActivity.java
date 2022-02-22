e02dbc95397732a4316d754e0e9db1ad436ebf7f
Roshan Pradhan <pradhanr@vcu.edu>
2022-02-22 16:02:40 -0500

    MultiActivity
    Added some function

---
+++ SquareOff/app/src/main/java/cmsc/com/squareoff/UI/GridActivity.java
@@ -0,0 +1,16 @@
+package cmsc.com.squareoff.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cmsc.com.squareoff.R;
+
+public class GridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid);
+    }
+}
