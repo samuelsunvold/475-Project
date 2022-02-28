37247ccb065336782617f80a9283690a63e97651
Sam Sunvold <sunvoldsj@vcu.edu>
2022-02-28 17:47:07 -0500

    Update winner screen/added a button on the 'SelectPlayerActivity' that takes you to the winner screen
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>

--- SquareOff/app/src/main/java/cmsc/com/squareoff/UI/SelectPlayersActivity.java
+++ SquareOff/app/src/main/java/cmsc/com/squareoff/UI/SelectPlayersActivity.java
@@ -11,13 +11,19 @@ import cmsc.com.squareoff.R;
 
 public class SelectPlayersActivity extends AppCompatActivity {
 
-
+    private Button dummyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_players);
 
+        dummyButton = findViewById(R.id.dummyButton);
+    }
 
+    // Method here just to bring you to Winner screen. Will be deleted later.
+    public void dummyButtonOnClickListener(View view) {
+        Intent winnerScreen = new Intent(this, WinnerActivity.class);
+        startActivity(winnerScreen);
     }
 }
--- SquareOff/app/src/main/java/cmsc/com/squareoff/UI/WinnerActivity.java
+++ SquareOff/app/src/main/java/cmsc/com/squareoff/UI/WinnerActivity.java
@@ -1,15 +1,29 @@
 package cmsc.com.squareoff.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import cmsc.com.squareoff.R;
 
 public class WinnerActivity extends AppCompatActivity {
 
+    private Button homeButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_winner);
+
+        homeButton = findViewById(R.id.homeButton);
+    }
+
+    // On click listener for the 'Home' button.
+    public void homeButtonOnClickListener(View view) {
+        Intent homeScreen = new Intent(this, HomeActivity.class);
+        startActivity(homeScreen);
     }
 }
