5c764eed1c19a6345cbd7ea35b6d2b62f5d66ca6
Sam Sunvold <sunvoldsj@vcu.edu>
2022-02-28 16:50:09 -0500

    Initial winner screen
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>

---
+++ SquareOff/app/src/main/java/cmsc/com/squareoff/UI/WinnerActivity.java
@@ -0,0 +1,15 @@
+package cmsc.com.squareoff.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cmsc.com.squareoff.R;
+
+public class WinnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_winner);
+    }
+}
