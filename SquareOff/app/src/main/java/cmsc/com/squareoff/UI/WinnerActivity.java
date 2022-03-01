da75c0e5ff921cbe4c2d8b569636787dc7eaa0b7
roshanpradhan <pradhanr@vcu.edu>
2022-02-28 23:12:04 -0500

    Adding Homebutton and some animation on winner screen

--- SquareOff/app/src/main/java/cmsc/com/squareoff/UI/WinnerActivity.java
+++ SquareOff/app/src/main/java/cmsc/com/squareoff/UI/WinnerActivity.java
@@ -6,24 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import cmsc.com.squareoff.R;
 
 public class WinnerActivity extends AppCompatActivity {
 
-    private Button homeButton;
+     ImageView homeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_winner);
 
-        homeButton = findViewById(R.id.homeButton);
+        homeButton = (ImageView) findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeScreen = new Intent(WinnerActivity.this, HomeActivity.class);
+                startActivity(homeScreen);
+            }
+        });
     }
 
-    // On click listener for the 'Home' button.
-    public void homeButtonOnClickListener(View view) {
-        Intent homeScreen = new Intent(this, HomeActivity.class);
-        startActivity(homeScreen);
-    }
+
 }
