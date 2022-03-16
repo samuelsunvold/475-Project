d744eb47a014659dc7a04d51f58a2a8ed6e87430
roshanpradhan <pradhanr@vcu.edu>
2022-03-16 01:01:22 -0400

    GameScreen sample

--- SquareOff/app/src/main/java/cmsc/com/squareoff/UI/SelectPlayersActivity.java
+++ SquareOff/app/src/main/java/cmsc/com/squareoff/UI/SelectPlayersActivity.java
@@ -17,6 +17,8 @@ public class SelectPlayersActivity extends AppCompatActivity {
 
     Button dummyButton;
     GridView gridView;
+    //Temp play Btn
+    Button play;
 
     int [] images = {R.drawable.team1,R.drawable.team2,R.drawable.team3,R.drawable.team4,
                     R.drawable.team5,R.drawable.team6,R.drawable.team7,R.drawable.team8,
@@ -28,6 +30,16 @@ public class SelectPlayersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_select_players);
 
         dummyButton = findViewById(R.id.dummyButton);
+
+        play = findViewById(R.id.playBtn);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent playBttn = new Intent(SelectPlayersActivity.this, so3x3.class);
+                startActivity(playBttn);
+            }
+        });
 
         gridView = findViewById(R.id.gridview);
         CustomAdapter customAdapter = new CustomAdapter(images,this);
@@ -41,4 +53,6 @@ public class SelectPlayersActivity extends AppCompatActivity {
         Intent winnerScreen = new Intent(this, WinnerActivity.class);
         startActivity(winnerScreen);
     }
+
+
 }
