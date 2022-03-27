dd05540d4af932831691bbd8a512f09b923335f1
Sam Sunvold <sunvoldsj@vcu.edu>
2022-03-27 18:51:23 -0400

    Added functionality for muting/unmuting music in settings page
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>

--- SquareUp/app/src/main/java/com/mrspd/squareoff/Ui/SettingsActivity.java
+++ SquareUp/app/src/main/java/com/mrspd/squareoff/Ui/SettingsActivity.java
@@ -2,15 +2,34 @@ package com.mrspd.squareoff.Ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.mrspd.squareoff.R;
+import com.mrspd.squareoff.Services.MusicService;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private boolean musicIsPlaying;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        musicIsPlaying = true;
+    }
+
+    public void musicButtonOnClickListener(View view) {
+        if(musicIsPlaying) {
+            stopService(new Intent(this, MusicService.class));
+            musicIsPlaying = false;
+        }
+        else {
+            startService(new Intent(this, MusicService.class));
+            musicIsPlaying = true;
+        }
     }
 }
