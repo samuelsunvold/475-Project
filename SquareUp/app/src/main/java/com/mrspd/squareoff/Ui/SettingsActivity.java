6e3c32161c949a04b902de18a7cb8f2eab0ebc3a
Sam Sunvold <sunvoldsj@vcu.edu>
2022-03-27 19:15:18 -0400

    Made button text change based on if music is unmuted or not
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>

--- SquareUp/app/src/main/java/com/mrspd/squareoff/Ui/SettingsActivity.java
+++ SquareUp/app/src/main/java/com/mrspd/squareoff/Ui/SettingsActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.mrspd.squareoff.R;
 import com.mrspd.squareoff.Services.MusicService;
@@ -14,21 +15,27 @@ public class SettingsActivity extends AppCompatActivity {
 
     private boolean musicIsPlaying;
 
+    private Button musicButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
         musicIsPlaying = true;
+
+        musicButton = findViewById(R.id.musicButton);
     }
 
     public void musicButtonOnClickListener(View view) {
         if(musicIsPlaying) {
             stopService(new Intent(this, MusicService.class));
+            musicButton.setText("Unmute Music");
             musicIsPlaying = false;
         }
         else {
             startService(new Intent(this, MusicService.class));
+            musicButton.setText("Mute Music");
             musicIsPlaying = true;
         }
     }
