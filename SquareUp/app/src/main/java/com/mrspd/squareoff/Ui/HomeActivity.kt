f8e04b0d9cd8e9be8229e3e6dab80a013e870275
Sam Sunvold <sunvoldsj@vcu.edu>
2022-03-28 16:06:20 -0400

    Added music icons as well as added functionality to stop/start music after clicking music icon
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>

--- SquareUp/app/src/main/java/com/mrspd/squareoff/Ui/HomeActivity.kt
+++ SquareUp/app/src/main/java/com/mrspd/squareoff/Ui/HomeActivity.kt
@@ -19,6 +19,9 @@ import kotlinx.android.synthetic.main.activity_home.*
 
 @Suppress("DEPRECATION")
 class HomeActivity : AppCompatActivity() {
+
+    var isMusicMuted: Boolean = false;
+
     @SuppressLint("CheckResult", "ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +31,7 @@ class HomeActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
 
-//////////////////////////////////////////////////
+        //////////////////////////////////////////////////
         Toasty.Config.getInstance()
                 .setToastTypeface(Typeface.createFromAsset(assets, "font.ttf"))
                 .allowQueue(true)
@@ -36,15 +39,25 @@ class HomeActivity : AppCompatActivity() {
         Toasty.custom(this@HomeActivity, R.string.custom_string, resources.getDrawable(R.drawable.background),
                 R.color.highlight1, R.color.green, Toasty.LENGTH_SHORT, false, true).show()
         Toasty.Config.reset()
-//////////////////////////////////////////////////
+        //////////////////////////////////////////////////
+
         start.setOnClickListener {
             intent = Intent(applicationContext, ChooseGridActivity::class.java)
             startActivity(intent)
         }
 
-        settingsButton.setOnClickListener {
-            intent = Intent(applicationContext, SettingsActivity::class.java)
-            startActivity(intent)
+        musicIcon.setOnClickListener {
+            val musicServiceIntent = Intent(applicationContext, MusicService::class.java)
+
+            isMusicMuted = if(isMusicMuted) {
+                startService(musicServiceIntent)
+                musicIcon.setImageResource(R.drawable.music_icon)
+                false;
+            } else {
+                stopService(musicServiceIntent)
+                musicIcon.setImageResource(R.drawable.muted_music_icon)
+                true;
+            }
         }
 //        tutorial.setOnClickListener {
 //            intent = Intent(applicationContext, TutorialActivity::class.java)
